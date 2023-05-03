64195fcd870ceb6a73987361bb368872ccb0ae50
anjalipoudel <sabnom77poudel@gmail.com>
2023-05-03 15:40:33 +0545

    basicclass

---
+++ day 4/basicclass.py
@@ -0,0 +1,39 @@
+'''
+python classes/object 
+python is an object oriented language.
+almost everything in python object,with its properties and methods in python,a class is a blueprint for creatingobjects.it  defines a set of attributes and methods  that the objects of that class will have
+'''
+'''
+class name:
+    x = "anjali"
+    print("name is :", x)
+    name
+'''
+
+
+#odd even check using class
+class Check:
+
+    def __init__(self, num):
+        self.num = num
+
+    def num_check(Self):
+        if self.num % 2 == 0:
+            print("even")
+        else:
+            print("odd")
+
+            def num_display(self):
+                print(self.num1)
+
+
+num = int(input("enter a number:"))
+n = Check(num)
+n.num_check()
+n.num_Check()
+
+
+#WAP to check given number is even or not
+class myclass:
+    def__int__(self, num)
+    self.num
